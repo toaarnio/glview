toaarnio/glview

commit d049d033bf5d8670842c915a8b3ea63c1992637d

Fix (some of the) code quality issues identified by pylint 2.3.1

argv.py
@@ -136,8 +136,7 @@ def floatstring(argname, default=None, accepted=None):
         except ValueError:
             if _isValid(argname, argstr, accepted):
                 return argstr
-            else:
-                sys.exit(-1)
+            sys.exit(-1)
     else:
         return default
 

glrenderer.py
@@ -3,11 +3,10 @@ import time                    # built-in library
 import struct                  # built-in library
 import threading               # built-in library
 import numpy as np             # pip install numpy
-import psutil                  # pip install psutil
 import moderngl                # pip install moderngl
 
 
-class GLRenderer(object):
+class GLRenderer:
 
     filter_nearest = (moderngl.NEAREST, moderngl.NEAREST)
     filter_linear = (moderngl.LINEAR_MIPMAP_LINEAR, moderngl.LINEAR)
@@ -23,11 +22,13 @@ class GLRenderer(object):
         self.files = files          # <FileList> Image files + metadata
         self.ctx = None             # <Context> OpenGL rendering context
         self.prog = None            # <Program> image renderer with zoom & pan
-        self.vao = None             # <VertexArray> planar surface
-        self.texture_filter = None  # filter_nearest or filter_linear
-        self.tile_colors = self.tile_debug_colors if verbose else self.tile_normal_colors
+        self.vbo = None             # <Buffer> xy vertex coords for 2D rectangle
+        self.vao = None             # <VertexArray> 2D rectangle vertices + shader
+        self.texture_filter = None  # filter_nearest' or filter_linear
         self.running = None
         self.render_thread = None
+        self.tPrev = None
+        self.tile_colors = self.tile_debug_colors if verbose else self.tile_normal_colors
 
     def init(self):
         # OpenGL window must already exist and be owned by this thread

imageprovider.py
@@ -8,7 +8,7 @@ import psutil                  # pip install psutil
 import imgio                   # pip install imgio
 
 
-class ImageProviderMT(object):
+class ImageProviderMT:
 
 
     def __init__(self, files, verbose=False):
@@ -59,7 +59,7 @@ class ImageProviderMT(object):
                         if self.files.images[idx] is None:
                             img = self._load_single(idx)
                             self.files.images[idx] = img
-                            nbytes += img.nbytes if type(img) == np.ndarray else 0
+                            nbytes += img.nbytes if isinstance(img, np.ndarray) else 0
                     else:
                         break
                 time.sleep(0.01)  # yield some CPU time to the UI thread
@@ -106,7 +106,7 @@ class ImageProviderMT(object):
     def _try(self, func):
         try:
             func()
-        except Exception as e:
+        except Exception as e:  # pylint: disable=broad-except
             self.running = False
             if self.verbose:
                 import traceback

pygletui.py
@@ -5,7 +5,7 @@ import pyglet                  # pip install pyglet
 import piexif                  # pip install piexif
 
 
-class PygletUI(object):
+class PygletUI:
 
     def __init__(self, files, numtiles, verbose=False):
         self.thread_name = "UIThread"
@@ -24,6 +24,7 @@ class PygletUI(object):
         self.fullscreen = False
         self.ui_thread = None
         self.event_loop = None
+        self.renderer = None
         self.texture_filter = "NEAREST"
         self.imgPerTile = [0, 0, 0, 0]
         self.gamma = False
@@ -108,14 +109,14 @@ class PygletUI(object):
     def _print_exif(self, filespec):
         try:
             exif_all = piexif.load(filespec)
-            exif_tags = {tag: name for name, tag in piexif.ExifIFD.__dict__.items() if type(tag) == int}
-            image_tags = {tag: name for name, tag in piexif.ImageIFD.__dict__.items() if type(tag) == int}
+            exif_tags = {tag: name for name, tag in piexif.ExifIFD.__dict__.items() if isinstance(tag, int)}
+            image_tags = {tag: name for name, tag in piexif.ImageIFD.__dict__.items() if isinstance(tag, int)}
             exif_dict = {exif_tags[name]: val for name, val in exif_all.pop("Exif").items()}
             image_dict = {image_tags[name]: val for name, val in exif_all.pop("0th").items()}
             merged_dict = {**exif_dict, **image_dict}
             print(f"EXIF data for {filespec}:")
             pprint.pprint(merged_dict)
-        except piexif._exceptions.InvalidImageDataError as e:
+        except piexif.InvalidImageDataError as e:
             print(f"Failed to extract EXIF metadata from {filespec}: {e}")
 
 
@@ -229,7 +230,7 @@ class PygletUI(object):
     def _try(self, func):
         try:
             func()
-        except Exception as e:
+        except Exception as e:  # pylint: disable=broad-except
             self.running = False
             self.event_loop.has_exit = True
             if self.verbose:

setup.cfg
@@ -1,0 +1,7 @@
+[pycodestyle]
+show-source = True
+max-line-length = 120
+ignore = E303,E241,E731
+
+[pylint]
+disable = C0103,C0326

commit 3be149f38fbefd6fcd0d2a2e560f1b2cbc391230

Fix a regression caused by a typo in commit d049d033

argv.py
@@ -203,7 +203,7 @@ def _isValid(argname, arg, validArgs=None, condition=None):
             print(f"Invalid value for '{argname}': '{arg}' is not in the set {validArgs}.")
             return False
     if condition is not None:
-        validator = eval("lambda v: {condition}")  # pylint: disable=eval-used
+        validator = eval(f"lambda v: {condition}")  # pylint: disable=eval-used
         if validator(arg) is not True:
             print(f"Invalid value for '{argname}': '{arg}' does not satisfy '{condition}'.")
             return False