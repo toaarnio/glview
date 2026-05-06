9772733c73b6cfbe651221835d6adbe8074f3632
Tomi Aarnio <tomi.p.aarnio@gmail.com>
2026-05-06 11:46:49 +0300

    cleanup: Fix file permissions 755 => 644

--- glview/argv.py
+++ glview/argv.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env -S python3 -B
-
 """
 A collection of simple command-line parsing functions.
 
--- glview/glview.py
+++ glview/glview.py
@@ -1,5 +1,3 @@
-#!/usr/bin/python3 -B
-
 """
 The 'glview' command-line application.
 """
